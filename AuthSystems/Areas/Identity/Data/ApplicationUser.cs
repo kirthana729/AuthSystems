8f0a6c0a715d79b29e70f5385762ac27c6b011f1
G.Kirthana <kirthanakiro74@gmail.com>
2023-09-15 06:37:54 +0530

    Add project files.

---
+++ AuthSystems/Areas/Identity/Data/ApplicationUser.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+
+namespace AuthSystems.Areas.Identity.Data;
+
+// Add profile data for application users by adding properties to the ApplicationUser class
+public class ApplicationUser : IdentityUser
+{
+	[PersonalData]
+	[Column(TypeName = "nvarchar(100)")]
+	public string FirstName { get; set; }
+
+	[PersonalData]
+	[Column(TypeName = "nvarchar(100)")]
+	public string LastName { get; set; }
+
+	//public string UserId { get; set; } = Guid.NewGuid().ToString().Substring(0, 4);
+	public string UserId { get; set; }
+}
+
